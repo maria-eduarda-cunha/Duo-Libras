842fd556bcb46494d169469074ec1d53ba5f39b9
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2025-11-09 17:03:15 +0000

    [Frontend] fix: rota criar-conta

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './member/login/login.component';
 import { UserComponent } from './user/user/user.component';
+import { SignInComponent } from './member/sign-in/sign-in.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'criar-conta', component: SignInComponent },
     { path: 'user', component: UserComponent, loadChildren: () => import('./user/user.module').then(m => m.UserModule)},
 ];
 
