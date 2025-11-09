f8192eb35ecdb89b69c7f3bf51209b92f9274648
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2025-11-09 20:43:59 +0000

    [Backend] fix: quiz

--- backend/controllers/quiz.py
+++ backend/controllers/quiz.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify, request
 from models.quiz import Quiz  # seu modelo no MongoDB
 
-quiz_bp = Blueprint('quiz', __name__)
+quiz_bp = Blueprint('quiz_bp', __name__)
 
-@quiz_bp.route('/api/quiz/<modulo>', methods=['GET'])
+@quiz_bp.route('/quiz/<modulo>', methods=['GET'])
 def get_quiz_by_modulo(modulo):
     quiz = Quiz.objects(modulo=modulo).first()
     if not quiz:
