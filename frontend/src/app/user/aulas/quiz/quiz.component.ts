0a21b837d0ac74a2446672d201e1c6ab43b9afca
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2025-11-09 20:13:36 +0000

    [Frontend] fix: aparência módulos

--- frontend/src/app/user/aulas/quiz/quiz.component.ts
+++ frontend/src/app/user/aulas/quiz/quiz.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent {
-  
+export class QuizComponent implements OnInit{
+  moduloSelecionado = '';
+  gifSrc = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.moduloSelecionado = this.route.snapshot.paramMap.get('moduloSelecionado') || '';
+    this.gifSrc = `/assets/aula-${this.moduloSelecionado}.mp4`;
+    console.log(this.gifSrc)
+  }
+
   selecionarOpcao(opcao: any){
 
   }
