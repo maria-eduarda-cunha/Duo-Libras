319fc358a7822fad297a6828d1c0ed55326d3d59
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2025-11-10 16:59:05 +0000

    [Backend] fix: quiz

--- backend/controllers/quiz.py
+++ backend/controllers/quiz.py
@@ -6,8 +6,11 @@ quiz_bp = Blueprint('quiz_bp', __name__)
 @quiz_bp.route('/<modulo>', methods=['GET'])
 def get_quiz_by_modulo(modulo):
     quiz = Quiz.objects(modulo=modulo).first()
-    print(quiz)
+    print(quiz['id'])
     if not quiz:
         print('❌ Módulo não encontrado')
         return jsonify({'error': 'Módulo não encontrado'}), 404
-    return jsonify(quiz), 200
+
+    quiz_dict = quiz.to_mongo().to_dict()
+    quiz_dict['_id'] = str(quiz.id)
+    return jsonify(quiz_dict)
