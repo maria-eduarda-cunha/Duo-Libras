5ee15b861d99db11bb46217f515f5fe74dddb799
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2026-09-01 21:15:00 +0000

    [Backend] fix: config render

--- backend/config.py
+++ backend/config.py
@@ -1,21 +1,14 @@
 import os
-import streamlit as st
-
-def get_secret(key):
-    try:
-        return st.secrets[key]
-    except Exception:
-        return os.getenv(key)
 
 # DB INFO
-db_mongo = get_secret("DB_MONGO")
-col_mongo = get_secret("COL_MONGO")
+db_mongo = os.getenv('DB_MONGO')
+col_mongo = os.getenv('COL_MONGO')
 
-# CRYPTO
-fernet_key = get_secret('FERNET_KEY')
+#CRYPTO
+fernet_key = os.getenv('FERNET_KEY')
 
-# USER ADM
-root_email = get_secret("ROOT_EMAIL")
-root_password = get_secret("ROOT_PASSWORD")
-root_name = get_secret("ROOT_NAME")
-root_last_name = get_secret("ROOT_LAST_NAME")
+#USER ADM
+root_email = os.getenv('ROOT_EMAIL')
+root_password = os.getenv('ROOT_PASSWORD')
+root_name = os.getenv('ROOT_NAME')
+root_last_name = os.getenv('ROOT_LAST_NAME')
