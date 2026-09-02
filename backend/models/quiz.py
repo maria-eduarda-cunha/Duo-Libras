e82d8e8e5075bf2d8dfb7b6347b284b4eb8e00e4
Maria Eduarda Cunha <128634212+maria-eduarda-cunha@users.noreply.github.com>
2026-09-02 23:00:08 +0000

    [Backend] fix: estrutura dos dados de quiz

--- backend/models/quiz.py
+++ backend/models/quiz.py
@@ -2,22 +2,7 @@ from mongoengine import *
 
 class Quiz(Document):
     modulo = StringField(required=True, unique=True)
-
-    pergunta1 = StringField()
-    respostas1 = DictField()
-    gif1 = StringField()
-
-    pergunta2 = StringField()
-    respostas2 = DictField()
-    gif2 = StringField()
-
-    pergunta3 = StringField()
-    respostas3 = DictField()
-    gif3 = StringField()
-    
-    meta = {
-        'collection': 'quiz'
-    }
+    quiz = ListField()
 
     @staticmethod
     def get_quiz_by_modulo(modulo):
